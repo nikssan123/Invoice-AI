b64209f58f16e0b768ecfed0a0c004d7385ab4d5
Nikolay Lyutov <nikolay.lyutov@ventionteams.com>
2026-02-09 00:06:51 +0200

    fix: add english support

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,9 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import bg from './locales/bg.json';
+import en from './locales/en.json';
 
 i18n.use(initReactI18next).init({
-  resources: { bg: { translation: bg } },
+  resources: { bg: { translation: bg }, en: { translation: en } },
   lng: 'bg',
   fallbackLng: 'bg',
   interpolation: { escapeValue: false },
